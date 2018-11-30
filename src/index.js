675817b59247d8422ff7ab2aef99ea264b001eb7
unknown <swartalow@gmail.com>
2018-11-30 14:33:22 +0000

    local env setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class MarkdownPrev extends React.Component{
+    render(){
+        return (<div>Hej urwy i luje</div>)
+    }
+}
+ReactDOM.render(<MarkdownPrev />, document.getElementById('root'))
