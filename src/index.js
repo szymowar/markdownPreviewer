234a0ca821f347089c4be23d407b02fda0a4fc1a
unknown <swartalow@gmail.com>
2019-01-18 10:36:03 +0000

    last update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const marked = require('marked');
 /*
 User Story #1: I can see a textarea element with a corresponding id="editor".
 
@@ -31,49 +32,96 @@ Optional Bonus (you do not need to make this test pass): My markdown
 previewer interprets carriage returns and
 renders them as br (line break) elements.
 */
+const prvexample = `# Welcome to my React Markdown Previewer!
 
-class Editor extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleInput = this.handleInput.bind(this);
-    }
-    handleInput(){
-        let elem = document.getElementById("textinp");
-            elem = elem.value;
-        this.props.sendData(elem);
-    }
-    render(){
-        return(<div id="editor">
-                <form>
-                    <textarea onChange={this.handleInput} id="textinp" placeholder="Type text here...">
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}` + '</a>';
+}
+
+const Editor = (props) =>{
+        return(
+                <div id="editor-container" className="items">
+                    <div className="head">Editor</div>
+                    <textarea id="editor" onChange={props.handleInput} className="content" defaultValue={props.prv}>
                     </textarea>
-                </form>
                 </div>)
-    }
-}
-class Preview extends React.Component {
-    render(){
-        return (<div id="preview"  placeholder="Preview...">{this.props.prv}</div>)
-    }
-}
+            }
+
+
+const Preview = (props) => {
+        return (<div className="items">
+                    <div className="head">Preview
+                    </div>
+                    <div id="preview" className="content" dangerouslySetInnerHTML={{__html: marked(props.prv, {renderer: renderer})}}>
+                    </div>
+                </div>)
+                }
+
 class MarkdownPrev extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            inputEditor: ''
+            inputEditor: prvexample
         };
         this.handleEditorInput = this.handleEditorInput.bind(this);
+
     }
+
     handleEditorInput(val){
             this.setState({
-                inputEditor: val
+                inputEditor: val.target.value
             });
     }
-    
     render(){
         return (<div className="container">
-                <Editor sendData={this.handleEditorInput}/>
-                <Preview prv={this.state.inputEditor}/>
+                    <div className="title">Markdown Previewer</div>
+                    <Editor handleInput={this.handleEditorInput} prv={this.state.inputEditor}/>
+                    <Preview prv={this.state.inputEditor} />
             </div>)
     }
 }
